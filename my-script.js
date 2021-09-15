1f45ee6456ae391dc553e7fcfe58f2a8542253c8
Liem <liem@accurx.com>
2021-09-15 13:51:18 +0100

    Add a click me button

--- my-script.js
+++ my-script.js
@@ -4,3 +4,20 @@ const onStart = () => {
 
 
 onStart();
+
+
+function sprinkle() {
+    console.log("click sprinkle")
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti()
+}
+
+const clickMeButton = document.getElementById("surprise");
+if (clickMeButton) {
+    clickMeButton.addEventListener("click", handleClick);
+}
+
+function handleClick() {
+    console.log("click sprinkled");
+    sprinkle();
+}
