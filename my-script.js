a77c1e45cc24be959edcee26d0d072f138a63d13
Liem <liem@accurx.com>
2021-09-15 14:17:01 +0100

    Add more button
    Try rename this commit

--- my-script.js
+++ my-script.js
@@ -21,3 +21,19 @@ function handleClick() {
     console.log("handle click");
     sprinkle();
 }
+
+/**
+ * More button
+ */
+const clickMoreButton = document.getElementById("more");
+let count = 0;
+const handleMoreClick = () => {
+    console.log("more button")
+    clickMoreButton.classList.add(`more-${count}`);
+    count += 1;
+    sprinkle();
+}
+
+if (clickMoreButton) {
+    clickMoreButton.addEventListener("click", handleMoreClick);
+}
